b5445fd566b25ce1ff4d268e01cee41cc3a4b7c8
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-06-27 21:39:22 +0100

    initial commit

---
+++ Arrays/classic_questions.py
@@ -0,0 +1,21 @@
+"""
+Top Interview Questions 
+Easy
+"""
+
+def remove_duplicates(arr): 
+    k = len(set(arr)) 
+    return k 
+
+
+
+
+
+def main(): 
+    
+    print(remove_duplicates([1,1,2]))
+
+
+
+if __name__ == "__main__": 
+    main() 
