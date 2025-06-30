55db43760770cedc3a9426f586fcbf750fe34a0f
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-06-30 13:42:50 +0100

    more solutions

--- Arrays/classic_questions.py
+++ Arrays/classic_questions.py
@@ -24,14 +24,46 @@ def rotate(nums, k):
     nums = nums[::-1] 
     nums[:k] = nums[:k][::-1]
     nums[k:] = nums[k:][::-1]
+    print(nums)
     return nums 
 
+def single_num(arr):
+    if len(arr) == 1:
+        return arr[0] 
+    arr = sorted(arr) 
+    p, q = 0, 1
+    while q < len(arr): 
+        if arr[p] == arr[q]:
+        # if arr[p] ^ arr[q] == 0:
+            p += 2 
+            q += 2 
+        else:
+            break  
+    return arr[p]
+
+def intersect(nums1, nums2): 
+    result = []
+    for num in nums1: 
+        if num in nums2:
+            result.append(num)
+            nums2.remove(num)
+            if not nums2:
+                break
+    return result
 
 
 def main(): 
     
-    assert rotate([1,2,3,4,5,6,7], 3) == [5,6,7,1,2,3,4]
-    assert rotate([-1,-100,3,99], 2) == [3,99,-1,-100]
+    # assert intersect([1], [2,2,1]) == [1]
+    # assert intersect([1], [2,2]) == []
+    # assert intersect([1,2,2,1], [2,2]) == [2,2]
+    # assert intersect([4,9,5], [9,4,9,8,4]) == [4,9]
+    # assert single_num([1,2,1]) == 2 
+    # assert single_num([2,2,1]) == 1
+    # assert single_num([4,1,2,1,2]) == 4
+    # assert single_num([1]) == 1
+    # assert rotate([1,2,3,4,5,6,7], 3) == [5,6,7,1,2,3,4]
+    # assert rotate([-1,-100,3,99], 2) == [3,99,-1,-100]
     # assert max_profit([7,1,5,3,6,4]) == 7
     # assert max_profit([1,2,3,4,5]) == 4
     # assert max_profit([7,6,4,3,1]) == 0
