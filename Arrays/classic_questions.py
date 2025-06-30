1d0cef3fef08d2af36751a784a465088aefa80a0
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-06-30 22:34:05 +0100

    more solutions

--- Arrays/classic_questions.py
+++ Arrays/classic_questions.py
@@ -63,11 +63,20 @@ def move_zeroes(nums):
             nums.append(nums.pop(i))
     return nums 
 
+def two_sum(nums, target): 
+    
+    for i in range(len(nums)-1): 
+        for j in range(i+1, len(nums)):
+            if (nums[i] + nums[j]) == target:
+                return [i, j] 
 
 
 
 def main(): 
     
+    assert two_sum([2,7,11,15], 9) == [0,1]
+    assert two_sum([3,2,4], 6) == [1,2]
+    assert two_sum([3,3], 6) == [0,1]
     # assert move_zeroes([0,1,0,3,12]) == [1,3,12,0,0]
     # assert move_zeroes([0]) == [0]
     # assert plus_one([1,2,3]) == [1,2,4]
