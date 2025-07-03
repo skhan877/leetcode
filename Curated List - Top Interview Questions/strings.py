5bab1b17f92d66ec2452d14f18c3568b4f1df906
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-07-03 22:17:07 +0100

    more solutions

--- Curated List - Top Interview Questions/strings.py
+++ Curated List - Top Interview Questions/strings.py
@@ -13,11 +13,46 @@ def reverse_string(s):
         j -= 1 
     return s
 
+def reverse_int(x: int) -> int: 
+    lst_x = list(str(x))
+    if lst_x[0] == "-":
+        result = int("-" + "".join(lst_x[::-1][:-1]))
+    else:
+        result = int("".join(lst_x[::-1]))
+
+    if result >= -2**31 and result <= (2**31)-1: 
+        return result 
+    else:
+        return 0 
+
+def first_unique_ch(s): 
+    from collections import Counter 
+    counts = Counter(list(s))
+    idxs = [] 
+    for k, v in counts.items(): 
+        if v == 1:
+            idxs.append(list(s).index(k))
+    return min(idxs) if idxs else -1 
+
+
+    
 
 def main(): 
+
+    assert first_unique_ch("leetcode") == 0 
+    assert first_unique_ch("aabb") == -1
+    assert first_unique_ch("loveleetcode") == 2
+
+    assert reverse_int(-51) == -15
+    assert reverse_int(120) == 21 
     
     assert reverse_string(["h","e","l","l","o"]) == ["o","l","l","e","h"]
     assert reverse_string(["H","a","n","n","a","h"]) == ["h","a","n","n","a","H"]
+    
+
+
+
+
 
 
 if __name__ == "__main__":
