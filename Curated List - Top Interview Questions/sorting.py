a17e71d6181228bbb0ed229c9f7f92dd57ac3f6a
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-07-10 20:59:46 +0100

    sorting problems initial commit

---
+++ Curated List - Top Interview Questions/sorting.py
@@ -0,0 +1,19 @@
+"""
+Top Interview Questions 
+Easy
+
+Sorting and Searching
+"""
+
+def merge(): 
+    pass 
+
+
+
+def main(): 
+    # assert merge() == 
+    pass 
+
+
+if __name__ == "__main__":
+    main()
