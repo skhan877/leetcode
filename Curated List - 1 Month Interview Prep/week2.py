60d76dcbecdd1722d0220bce5ab60f8190a9b680
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-07-21 12:50:17 +0100

    more solutions

--- Curated List - 1 Month Interview Prep/week2.py
+++ Curated List - 1 Month Interview Prep/week2.py
@@ -8,9 +8,25 @@ def sockMerchant(n, ar):
         pairs += v // 2 
     return pairs 
 
+def findZigZagSequence(a, n): 
+    a.sort()
+    mid = int(n/2)
+    a[mid], a[n-1] = a[n-1], a[mid] 
+
+    st = mid + 1
+    ed = n - 2  
+    while(st < ed): 
+        a[st], a[ed] = a[ed], a[st] 
+        st = st + 1
+        ed = ed - 1 
+
+    return a
 
 
 def main(): 
+
+    assert findZigZagSequence([2,3,5,1,4,9,7,6,8], 9) == [1,2,3,4,9,8,7,6,5]
+    assert findZigZagSequence([2,3,5,1,4], 5) == [1,2,5,4,3]
     
     assert sockMerchant(7, [1,2,1,2,1,3,2]) == 2
     assert sockMerchant(9, [10, 20, 10, 20, 10, 30, 50, 10, 20]) == 3
